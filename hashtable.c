8e1500ab9627d5d86c5b71c7ea792b13bf239bdb
Flavio <flavio.giacomozzi97@gmail.com>
2025-09-07 22:30:05 +0200

    Added new function insert_manual: insert an element in the table manually without hash function, directly with the index.

--- hashtable.c
+++ hashtable.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define N 10
 
@@ -24,23 +25,59 @@ Hashtable *create_table(int size){
     if(size>0){
         Hashtable *new_hash = (Hashtable*)malloc(sizeof(Hashtable));
         new_hash->size = size;
-        
+
         Bucket **new_table = (Bucket**)calloc(size, sizeof(Bucket*));
         new_hash->table = new_table;
-    }
+        return new_hash;
+     }
     else{
         printf("You inserted %d but is an invalid size number!", size);
         return NULL;
     }
-    return new_hash;
 }
 
+void insert_manual(Hashtable *ht, int index, int value, char *key){
+    if(!ht){
+        puts("Passed null hashtable");
+        return;
+    }
+    if(index >= ht->size && index < 0){ 
+        puts("Trying to access over the table bounds: index > talbe size.");
+        return;
+    }
+    if(!key){ 
+        puts("Key can't be null, it must be a string.");
+        return;
+    }
+    
+    Bucket *new_bucket = (Bucket*)malloc(sizeof(Bucket));
+    if(!new_bucket){
+        puts("New bucket allocation error.");
+        return;
+    }
+    new_bucket->value = value;
+    new_bucket->key = strdup(key); 
+    // strdup(char *) allocate char type of the copied string size... 
+    // ...and return pointer of the string with the copied string already inside.
+    new_bucket->next = NULL;
+
+    
+    if(ht->table[index]==NULL)
+        ht->table[index] = new_bucket;
+    else{
+        Bucket *last_bucket = ht->table[index];
+        while(last_bucket->next != NULL){
+            last_bucket = last_bucket->next;
+        }
+        last_bucket->next = new_bucket;
+    }
+}
 
 
 // MAIN
 
 int main(void){
-    
+
     Hashtable *hashtable1 = create_table(N);
-    
+
 }
